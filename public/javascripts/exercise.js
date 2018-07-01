31c343d576c430aea6afdd120c984ed91c845210
Rikimaru <aal.malamute@gmail.com>
2018-07-01 07:33:47 -0400

    added form

--- public/javascripts/exercise.js
+++ public/javascripts/exercise.js
@@ -24,6 +24,35 @@ function selectExercise(exerciseName) {
 }
 
 function changeFocusTo(exercise) {
-  let exerciseHeader = document.getElementById('currentExercise');
-  exerciseHeader.innerHTML = exercise.name;
+  console.log(exercise);
+
+  let exerciseNameHeader = document.getElementById('currentExerciseHeader');
+  let exerciseNameInput = document.getElementById('exerciseNameInput');
+  let exerciseAuthorInput = document.getElementById('exerciseAuthorInput');
+  let exerciseLevelSelector = document.getElementById('exerciseLevelSelector');
+  let exerciseTypeSelector = document.getElementById('exerciseTypeSelector');
+  let exerciseMuscleGroupMultiSelector = document.getElementById('exerciseMuscleGroupMultiSelector');
+  let instructionsTextArea = document.getElementById('instructionsTextArea');
+  let notesTextArea = document.getElementById('notesTextArea');
+
+  let instructionText = '';
+  exercise.instructions.forEach((step) => {
+    instructionText = instructionText.concat(step + '\n');
+  });
+
+  let noteText = '';
+  console.log(exercise.note);
+  exercise.note.forEach((line) => {
+    noteText = noteText.concat(line + '\n');
+  });
+
+  exerciseNameHeader.innerHTML = exercise.name;
+  exerciseNameInput.value = exercise.name;
+  exerciseAuthorInput.value = exercise.author;
+  exerciseLevelSelector.value = exercise.level;
+  exerciseTypeSelector.value = exercise.type;
+  exerciseMuscleGroupMultiSelector.value = ['Chest', 'Shoulders', 'Arms'];
+  instructionsTextArea.value = instructionText;
+  notesTextArea.value = noteText;
+
 }
